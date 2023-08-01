474de0bf5f06e71deaf7dd047ea755c710dd9d57
satyahurix095 <satya.ravichandrakumar@hurix.com>
2023-08-01 16:41:07 +0530

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 const peopleData = [{name: "Damon 🎶🎶",src: "https://e0.pxfuel.com/wallpapers/52/1008/desktop-wallpaper-damon-salvatore.jpg",},{name: "Stephen 🎶🎶",src: "https://upload.wikimedia.org/wikipedia/en/4/4b/Stefan_Salvatore.png"},{name: "Matt 🎶🎶",src: "https://m.media-amazon.com/images/M/MV5BMjE2NDYwMzIwNl5BMl5BanBnXkFtZTcwMDYyMjM5OA@@._V1_.jpg",},];
 export default function App() {
   return (
     <div className="App">
-      {peopleData.map((person)=> (<Wel name ={person.name} src = {person.src}/>))}
+      {peopleData.map((person)=> (<Wel name ={person.name}src= {person.src}/>))}
     </div>
   );
 }
@@ -14,6 +15,42 @@ function Wel({name,src}) {
     <div className= "cont">
       <img src={src}/>
       <h3><span>Hello</span>, {name}</h3>
+      <Count />
+      <ColorGame />
     </div>
   );
 }
+
+function Count(props){
+  const [like, setLike] = useState(0);
+  const [disLike, setDislike] = useState(0);
+  
+  return (
+    <div>
+      <button onClick={() => setLike(like + 1)}>👍 {like}</button>
+      <button onClick={() => setDislike(disLike + 1)}>👎 {disLike}</button>
+      {like >= disLike + 10  ? (<h4>your Cool</h4>) : null}
+      
+      <p><label for="file">likes and Dislikes</label><br/>
+      <progress completed = {like} bgColor = "red"></progress></p>
+    </div>
+  )
+}
+function ColorGame() {
+    const [bg, setbgcol] = useState("black");
+    const styles = {
+        background: bg,
+        };
+          return (
+          <div>
+            <input
+                style={styles}
+                onChange={(event) => setbgcol(event.target.value)}
+                type="text"
+                placeholder="please type a color"
+                value = {bg}
+            />
+  </div>
+  );
+ }
+
